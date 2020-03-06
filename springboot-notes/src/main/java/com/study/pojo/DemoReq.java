c419bec9ebd03210546bab95d644b9bce754bd0c
shangcj@unitedcloud.net <shangcj@unitedcloud.net>
2020-03-06 18:13:42 +0800

    bean validator  test

--- springboot-notes/src/main/java/com/study/pojo/DemoReq.java
+++ springboot-notes/src/main/java/com/study/pojo/DemoReq.java
@@ -1,10 +1,23 @@
 package com.study.pojo;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+
 public class DemoReq {
 
 
+	@NotEmpty
+	@Range(min = 1,max = 10)
 	String reqName;
+	@Size(max = 100,min=2)
 	String reqId;
+	
+	@Min(10)
+	@Max(121)
 	int age;
 	@Override
 	public String toString() {
